c40b3c24159ed3f53ded2eeeb04765050c7879a3
Abdullah-Al-Foysal <43477963+Abdullahfoysal@users.noreply.github.com>
2023-01-13 19:21:35 +0600

    motor run in same direction

--- motorMain.py
+++ motorMain.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
 motor1A = 8
 motor1B =10
 motor1E=12
-motor2A = 7
-motor2B =5
-motor2E=3
 
-GPIO.setwarnings(False)
+motor2E=33
+motor2A = 35
+motor2B =37
+
+
+GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(motor1A,GPIO.OUT)
 GPIO.setup(motor1B,GPIO.OUT)
@@ -16,35 +19,48 @@ GPIO.setup(motor2A,GPIO.OUT)
 GPIO.setup(motor2B,GPIO.OUT)
 GPIO.setup(motor2E,GPIO.OUT)
 
-p = GPIO.PWM(motor1E,100)
-q = GPIO.PWM(motor2E,100)
+p1 = GPIO.PWM(motor1E,100)
+p2 = GPIO.PWM(motor2E,100)
+
+
+
 
 def motor(a,b):
-    if (a>=0):
-        GPIO.output(motor1A,True)
-        GPIO.output(motor1B,False)
-
-    else:
+    if (a>=0 and b>=0):
         GPIO.output(motor1A,False)
         GPIO.output(motor1B,True)
-
-    if (b>=0):
-        GPIO.output(motor2A,True)
-        GPIO.output(motor2B,False)
-
-    else:
+        
         GPIO.output(motor2A,False)
         GPIO.output(motor2B,True)
-    p.start(abs(a))
-    q.start(abs(b))
 
-while True:
-    motor(100,100)
-    sleep(5)
-    motor(-100,-100)
-    sleep(5)
-    motor(10,10)
-    sleep(5)
+    else:
+        
+        GPIO.output(motor1A,True)
+        GPIO.output(motor1B,False) 
+        
+        GPIO.output(motor2A,True)
+        GPIO.output(motor2B,False)
+        
+    p1.start(abs(a))
+    p2.start(abs(b))
+   
 
+   
 
-GPIO.cleanup()
+try:
+    while True:
+        print("Working")
+        motor(100,100)
+        sleep(3)
+        motor(-100,-100)
+        sleep(3)
+        #motor2Run()
+        
+except KeyboardInterrupt:
+    print("Keyboard Interrupt occured")
+except:
+    print("Other exception occured")
+finally:
+    GPIO.cleanup()
+    print("clean exit")
+               
